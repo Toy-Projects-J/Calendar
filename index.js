96c9a5616d63eb79f09950291b99720094203c00
stooooopu <jyg5193@naver.com>
2022-04-21 17:30:38 +0900

    fix stopu

--- index.js
+++ index.js
@@ -3,4 +3,19 @@ const date = new Date();
 const viewYear = date.getFullYear();
 const viewMonth = date.getMonth();
 
-document.querySelector('.year-month-nav').textContent
+// console.log(viewYear); console.log(viewMonth) ;
+document.querySelector('.current-year-month-wrap').textContent = `${viewYear}년 ${viewMonth + 1}월`;
+
+const prevLast = new Date(viewYear, viewMonth, 0);
+const thisLast = new Date(viewYear, viewMonth + 1, 0);
+
+// console.log(preLast); console.log(thisLast);
+
+const PLDate = prevLast.getDate();
+const PlDay = prevLast.getDay();
+
+// console.log(PLDate); console.log(PlDay);
+
+const TLDate = thisLast.getDate();
+console.log(TLDate)
+
